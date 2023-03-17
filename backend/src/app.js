894b7cd9d59d031a9fbb61a0037c4ae44156a6bb
LAURE JANIN <ljanin51@gmail.com>
2023-03-17 10:35:56 +0100

    npm run fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,6 +26,20 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
 
 // Serve the public folder for public resources
